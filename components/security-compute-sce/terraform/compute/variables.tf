c63b9e54708e408a419022fd28dc26fc50a9b575
sahoa <ashishkumar.sahoo@in.pega.com>
2026-01-29 18:14:31 +0530

    Updated the logic.

--- components/security-compute-sce/terraform/compute/variables.tf
+++ components/security-compute-sce/terraform/compute/variables.tf
@@ -3,6 +3,7 @@
 variable "s3_bucket_arn" {
   description = "The ARN of the S3 bucket that this Lambda function will consume."
   type        = string
+  default     = "arn://"
 }
 
 variable "lambda_function_name" {
